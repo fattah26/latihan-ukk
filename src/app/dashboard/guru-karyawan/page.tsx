e4513bf2de67c4ed2216157803352b2760888bb9
fattah26 <fattaharif87@gmail.com>
2025-01-20 15:11:07 +0700

    fix: page guru

--- src/app/dashboard/guru-karyawan/page.tsx
+++ src/app/dashboard/guru-karyawan/page.tsx
@@ -1,20 +1,11 @@
 "use client";
-import DynamicTable from "@/components/Tables";
 
 export default function GuruPage(){
-
-
-  const header = [
-    { key: "id", label: "No", minWidth: 50 },
-    { key: "name", label: "Nama", minWidth: 150 },
-    { key: "jk", label: "Jenis Kelamin", minWidth: 100 },
-    { key: "noHp", label: "No HP", minWidth: 150 },
-  ];
 
   return (
     <div className="flex items-center">
       <div className="w-full bg-white">
-        <DynamicTable data={guru} header={header} />
+       <h1>Table guru</h1>
       </div>
     </div>
   );
